c93fb7080c7b0415d2afeb231909f3d38dc1e5ef
mportizlunyov <108703641+mportizlunyov@users.noreply.github.com>
2024-12-26 20:40:02 -0600

    created initial engine, printing out username/email combo.

--- javascript.d/engine.js
+++ javascript.d/engine.js
@@ -1,1 +1,46 @@
-// Main javascript engine to generate credentials
+// Main Javascript Engine to generate credentials
+
+// Method to generate a string. Can be called multiple times for multiple strings
+function generateString() {
+  // Return a substring
+  // // Generated via the Math.random()'s output,
+  // // converted into a string with Base36 (9 digits + 27 English characters),
+  // // getting the substring excluding the first 2 indexes ('0.')
+  return Math.random().toString(36).substring(2);
+}
+
+// Constant variable/object-method
+const credential = {
+  // Generates random username
+  username(extraLength = 1) {
+    let usernameToReturn = "";
+    // Anti-fuzzing/breaking to set extraLength parameter
+    if (extraLength < 1) {
+      extraLength = 1;
+    }
+    // Increase length of usernameToReturn as requested by user
+    for (let i = 0; i < extraLength; i++) {
+      usernameToReturn += generateString();
+    }
+    return usernameToReturn;
+  },
+
+  // Generates random email address
+  email(extraLength = 1) {
+    let emailToReturn = "";
+    // Anti-fuzzing/breaking to set extraLength parameter
+    if (extraLength < 1) {
+      extraLength = 1;
+    }
+    emailToReturn += credential.username(extraLength);
+    emailToReturn += "@";
+    emailToReturn += credential.username(extraLength);
+    emailToReturn += ".";
+    emailToReturn += credential.username(extraLength);
+    
+    return emailToReturn;
+  }
+}
+
+console.log(credential.username(1));
+console.log(credential.email(1));
